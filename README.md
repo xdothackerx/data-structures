7d08c5bf2f15cf2ade6e6062e8ef9f91cce09577
xdothackerx <xdothackerx@gmail.com>
2014-06-02 18:43:33 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+<h4>Project Title: Data Structures</h4>
+<h5>Author: Sam Camus (xdothackerx) </h5>
+
+This repo holds sample code for a number of classic data structures implemented in Ruby.
+
+Resources: http://en.wikipedia.org/wiki/Linked_list
+
+Collaborators:
---
+++ test/linked_list_test.rb
@@ -0,0 +1,24 @@
+feature "insert(node)" do
+  scenario "insert a node into the head of the list" do
+  end
+end
+
+feature "size()" do
+  scenario "return the length of the list" do
+  end
+end
+
+feature "search(value)" do
+  scenario "return the node containing 'value' in the list if present, else return nil" do
+  end
+end
+
+feature "remove(node)" do
+  scenario "remove the given node from the list, at any location, and return the object's value. If it isn't found, return nil" do
+  end
+end
+
+feature "to_s()" do
+  scenario "print the list as a CSV string" do
+  end
+end
