0a6ae73e5da8e6259b17d93b15c5a0dc8b708c4f
xdothackerx <xdothackerx@gmail.com>
2014-06-11 22:14:29 -0700

    Implement quick sort, with tests.

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,133 +1,102 @@
 class Array
 
-  # def self.ins_sort(arr)
-  #   (1..arr.length-1).each do |n|
-  #     insertion_ind = n
-  #     insertion_val = arr.delete_at(insertion_ind)
-  #     while insertion_ind > 0 && insertion_val < arr[insertion_ind-1]
-  #       insertion_ind -= 1
-  #     end
-  #     arr.insert(insertion_ind, insertion_val)
-  #   end
-  #   return arr
-  # end
-
-  # def self.merge_sort(arr)
-  #   return arr if arr.empty? || arr.sorted?
-  #   arr = arr.combination(1).to_a
-  #   merge = []
-  #   until arr.size == 1
-  #     left = arr.shift
-  #     right = arr.shift
-  #     until left.size.zero? || right.size.zero?
-  #       if left.first < right.first
-  #         merge << left.shift
-  #       else
-  #         merge << right.shift
-  #       end
-  #     end
-  #     if left.size > 0
-  #       merge += left
-  #       left = []
-  #     else
-  #       merge += right
-  #       right = []
-  #     end
-  #     arr << merge
-  #     merge = []
-  #   end
-  #   arr.flatten
-  # end
-
-  def self.quick_sort(arr)
-    left = 0
-    right = arr.size-1
-    pivot = arr.size/2.0.round
-    until left == pivot-1 || right == pivot+1
-      until arr[left] > arr[pivot] || left == pivot-1
-        if arr[left] < arr[pivot]
-        left += 1
-        end
+  def self.ins_sort(arr)
+    (1..arr.length-1).each do |n|
+      insertion_ind = n
+      insertion_val = arr.delete_at(insertion_ind)
+      while insertion_ind > 0 && insertion_val < arr[insertion_ind-1]
+        insertion_ind -= 1
       end
-      until arr[right] < arr[pivot] || right == pivot+1
-        if arr[right] > arr[pivot]
-        right -= 1
-        end
-      end
-      # puts arr[pivot]
-      # puts arr[left]
-      # puts arr[right]
-      # puts arr.inspect
-      arr[right], arr[left] = arr[left], arr[right]
+      arr.insert(insertion_ind, insertion_val)
     end
-    until left == pivot-1
-      if arr[left] > arr[right]
-       arr[left], arr[right] = arr[right], arr[left]
-
-      end
-      if arr[left] > arr[pivot]
-        arr[left], arr[pivot] = arr[pivot], arr[left]
-
-      end
-      left += 1
-    end
-    until right == pivot+1
-      if arr[left] > arr[right]
-       arr[left], arr[right] = arr[right], arr[left]
-
-      end
-      if arr[right] < arr[pivot]
-        arr[right], arr[pivot] = arr[pivot], arr[right]
-      end
-      right -= 1
-
-    end
-    until right == arr.size-1
-      if arr[left] > arr[right]
-       arr[left], arr[right] = arr[right], arr[left]
-
-      end
-      if arr[right] < arr[pivot]
-        arr[right], arr[pivot] = arr[pivot], arr[right]
-        pivot += 1
-        right += 1 unless right == arr.size
-      else
-        right += 1 unless right == arr.size
-      end
-    end
-    if arr[left] > arr[right]
-       arr[left], arr[right] = arr[right], arr[left]
-
-    end
-    puts arr[pivot]
     return arr
   end
 
-  #   right = arr.size-1
-  #   if arr[left] < arr[right]
-  #     pivot = arr.size/2.0.round
-  #     new_pivot = part(arr, left, right, pivot)
-  #     Array.quick_sort(arr[left..new_pivot-1])
-  #     Array.quick_sort(arr[new_pivot..right])
-  #   end
-  #   return arr
-  # end
+  def self.merge_sort(arr)
+    return arr if arr.empty? || arr.sorted?
+    arr = arr.combination(1).to_a
+    merge = []
+    until arr.size == 1
+      left = arr.shift
+      right = arr.shift
+      until left.size.zero? || right.size.zero?
+        if left.first < right.first
+          merge << left.shift
+        else
+          merge << right.shift
+        end
+      end
+      if left.size > 0
+        merge += left
+        left = []
+      else
+        merge += right
+        right = []
+      end
+      arr << merge
+      merge = []
+    end
+    arr.flatten
+  end
 
-  # def self.part(arr, left, right, pivot)
-  #   arr[right], arr[pivot] = arr[pivot], arr[right]
-  #   store_index = left
-  #   until store_index = right
-  #     if array[left] < array[pivot]
-  #       array[left], array[pivot] = array[pivot], array[left]
-  #       store_index += 1
-  #     end
-  #     arr[right], arr[store_index] = arr[store_index], arr[right]
-  #   end
-  #   return arr[store_index]
-  # end
+  def quick_sort(left = 0, right = self.size-1)
 
-  # def sorted?
-  #   each_cons(2).all? { |a, b| (a <=> b) <= 0 }
-  # end
+    if left < right
+      pivot = rand(left...right)
+      new_pivot = part(left, right, pivot)
+      # binding.pry
+      self.quick_sort(left, new_pivot-1)
+      self.quick_sort(new_pivot+1, right)
+    end
+    self
+  end
+
+  def part(left, right, pivot)
+    pivot_value = self[pivot]
+    self[right], self[pivot] = self[pivot], self[right]
+    store_index = left
+    (left...right).each do |n|
+      if self[n] < pivot_value
+        self[n], self[store_index] = self[store_index], self[n]
+        store_index += 1
+      end
+    end
+    self[right], self[store_index] = self[store_index], self[right]
+    store_index
+  end
+
+  def sorted?
+    each_cons(2).all? { |a, b| (a <=> b) <= 0 }
+  end
 
 end
+
+
+ #   left = 0
+  #   right = arr.size-1
+  #   pivot = arr.size/2.0.round
+  #   until left == arr.size-1
+  #     if arr[left] > arr[pivot]
+  #       arr[right], arr[left] = arr[left], arr[right]
+  #       left+=1
+  #       right-=1
+  #     else
+  #       left += 1
+  #     end
+  #     puts arr[pivot]
+  #     puts arr[left]
+  #     puts arr[right]
+  #     puts arr.inspect
+  #   end
+  #   # until pivot == 0
+  #   #   if arr[pivot] > arr[pivot+1]
+  #   #     arr[pivot], arr[pivot+1] = arr[pivot+1], arr[pivot]
+  #   #     pivot += 1
+  #   #   end
+  #   #   puts arr[pivot]
+  #   #   puts arr[left]
+  #   #   puts arr[right]
+  #   #   puts arr.inspect
+  #   # end
+  #   return arr
+  # end
--- test/sort_test.rb
+++ test/sort_test.rb
@@ -2,10 +2,12 @@ require "minitest/spec"
 require "minitest/autorun"
 require "benchmark"
 require "./lib/sort"
+require "pry"
+require "pry-nav"
 
 def gen_nums(type)
   if type == "random"
-    (1..10).to_a.shuffle
+    (1..100).to_a.shuffle
   elsif type == "nearly-ordered"
     nums = (1..100).to_a
     move = nums[0]
@@ -23,77 +25,73 @@ def gen_nums(type)
   end
 end
 
-# describe "Insertion Sort" do
-#   it "Sorts an array of random numbers from least to greatest." do
-#     input = Array.ins_sort(gen_nums "random")
-#     input.must_equal (1..100).to_a
-#   end
-#   it "Sorts an array of nearly sorted numbers" do
-#     input = Array.ins_sort(gen_nums "nearly-ordered")
-#     input.must_equal (1..100).to_a
-#   end
-#   it "Sorts an array of reversed sorted numbers" do
-#     input = Array.ins_sort(gen_nums "reverse")
-#     input.must_equal (1..100).to_a
-#   end
-#   it "Sorts an array of numbers with few unique values" do
-#     input = Array.ins_sort(gen_nums "few unique")
-#     input.must_equal gen_nums("few unique").sort
-#   end
-# end
+describe "Insertion Sort" do
+  it "Sorts an array of random numbers from least to greatest." do
+    input = Array.ins_sort(gen_nums "random")
+    input.must_equal (1..100).to_a
+  end
+  it "Sorts an array of nearly sorted numbers" do
+    input = Array.ins_sort(gen_nums "nearly-ordered")
+    input.must_equal (1..100).to_a
+  end
+  it "Sorts an array of reversed sorted numbers" do
+    input = Array.ins_sort(gen_nums "reverse")
+    input.must_equal (1..100).to_a
+  end
+  it "Sorts an array of numbers with few unique values" do
+    input = Array.ins_sort(gen_nums "few unique")
+    input.must_equal gen_nums("few unique").sort
+  end
+end
 
-# describe "Merge Sort" do
-#   it "Sorts an array of random numbers from least to greatest." do
-#     input = Array.merge_sort(gen_nums "random")
-#     input.must_equal (1..100).to_a
-#   end
-#   it "Sorts an array of nearly sorted numbers" do
-#     input = Array.merge_sort(gen_nums "nearly-ordered")
-#     input.must_equal (1..100).to_a
-#   end
-#   it "Sorts an array of reversed sorted numbers" do
-#     input = Array.merge_sort(gen_nums "reverse")
-#     input.must_equal (1..100).to_a
-#   end
-#   it "Sorts an array of numbers with few unique values" do
-#     input = Array.merge_sort(gen_nums "few unique")
-#     input.must_equal gen_nums("few unique").sort
-#   end
-# end
+describe "Merge Sort" do
+  it "Sorts an array of random numbers from least to greatest." do
+    input = Array.merge_sort(gen_nums "random")
+    input.must_equal (1..100).to_a
+  end
+  it "Sorts an array of nearly sorted numbers" do
+    input = Array.merge_sort(gen_nums "nearly-ordered")
+    input.must_equal (1..100).to_a
+  end
+  it "Sorts an array of reversed sorted numbers" do
+    input = Array.merge_sort(gen_nums "reverse")
+    input.must_equal (1..100).to_a
+  end
+  it "Sorts an array of numbers with few unique values" do
+    input = Array.merge_sort(gen_nums "few unique")
+    input.must_equal gen_nums("few unique").sort
+  end
+end
 
 describe "Quick Sort" do
   it "Sorts an array of random numbers from least to greatest." do
-    input = Array.quick_sort(gen_nums "random")
-    input.must_equal (1..10).to_a
+    gen_nums("random").quick_sort.must_equal (1..100).to_a
   end
-  # it "Sorts an array of nearly sorted numbers" do
-  #   input = Array.quick_sort(gen_nums "nearly-ordered")
-  #   input.must_equal (1..100).to_a
-  # end
-  # it "Sorts an array of reversed sorted numbers" do
-  #   input = Array.quick_sort(gen_nums "reverse")
-  #   input.must_equal (1..100).to_a
-  # end
-  # it "Sorts an array of numbers with few unique values" do
-  #   input = Array.quick_sort(gen_nums "few unique")
-  #   input.must_equal gen_nums("few unique").sort
-  # end
+  it "Sorts an array of nearly sorted numbers" do
+    gen_nums("nearly-ordered").quick_sort.must_equal (1..100).to_a
+  end
+  it "Sorts an array of reversed sorted numbers" do
+    gen_nums("reverse").quick_sort.must_equal (1..100).to_a
+  end
+  it "Sorts an array of numbers with few unique values" do
+    gen_nums("few unique").quick_sort.must_equal gen_nums("few unique").sort
+  end
 end
 
 Minitest.after_run {
-  # puts "Insertion Sort:"
-  # puts "Nearly-Ordered: #{Benchmark.measure{Array.ins_sort gen_nums("nearly-ordered")}}"
-  # puts "Few Unique: #{Benchmark.measure{Array.ins_sort gen_nums("few unique")}}"
-  # puts "Random Order: #{Benchmark.measure{Array.ins_sort gen_nums("random")}}"
-  # puts "Reverse Order: #{Benchmark.measure{Array.ins_sort gen_nums("reverse")}}"
-  # puts "\n Merge Sort:"
-  # puts "Nearly-Ordered: #{Benchmark.measure{Array.merge_sort gen_nums("nearly-ordered")}}"
-  # puts "Few Unique: #{Benchmark.measure{Array.merge_sort gen_nums("few unique")}}"
-  # puts "Random Order: #{Benchmark.measure{Array.merge_sort gen_nums("random")}}"
-  # puts "Reverse Order: #{Benchmark.measure{Array.merge_sort gen_nums("reverse")}}"
-  # puts "\n Quick Sort:"
-  # puts "Nearly-Ordered: #{Benchmark.measure{Array.quick_sort gen_nums("nearly-ordered")}}"
-  # puts "Few Unique: #{Benchmark.measure{Array.quick_sort gen_nums("few unique")}}"
-  # puts "Random Order: #{Benchmark.measure{Array.quick_sort gen_nums("random")}}"
-  # puts "Reverse Order: #{Benchmark.measure{Array.quick_sort gen_nums("reverse")}}"
+  puts "\n Insertion Sort:"
+  puts "Nearly-Ordered: #{Benchmark.measure{Array.ins_sort gen_nums("nearly-ordered")}}"
+  puts "Few Unique: #{Benchmark.measure{Array.ins_sort gen_nums("few unique")}}"
+  puts "Random Order: #{Benchmark.measure{Array.ins_sort gen_nums("random")}}"
+  puts "Reverse Order: #{Benchmark.measure{Array.ins_sort gen_nums("reverse")}}"
+  puts "\n Merge Sort:"
+  puts "Nearly-Ordered: #{Benchmark.measure{Array.merge_sort gen_nums("nearly-ordered")}}"
+  puts "Few Unique: #{Benchmark.measure{Array.merge_sort gen_nums("few unique")}}"
+  puts "Random Order: #{Benchmark.measure{Array.merge_sort gen_nums("random")}}"
+  puts "Reverse Order: #{Benchmark.measure{Array.merge_sort gen_nums("reverse")}}"
+  puts "\n Quick Sort:"
+  puts "Nearly-Ordered: #{Benchmark.measure{gen_nums("nearly-ordered").quick_sort}}"
+  puts "Few Unique: #{Benchmark.measure{gen_nums("few unique").quick_sort}}"
+  puts "Random Order: #{Benchmark.measure{gen_nums("random").quick_sort}}"
+  puts "Reverse Order: #{Benchmark.measure{gen_nums("reverse").quick_sort}}"
 }
